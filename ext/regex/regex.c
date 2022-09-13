c4f487eebe98f3e2494b3a4af6fb1bdda2129140
pejman <pejman.hkh@gmail.com>
2022-09-13 13:33:48 +0430

    some changes for regex extension

--- ext/regex/regex.c
+++ ext/regex/regex.c
@@ -20,7 +20,15 @@ do_regex( variable *ths, variable *pattern, variable *ex ) {
 	size_t pattern_size = strlen(pattern->val);
 	uint32_t options = 0;
 
-	re = pcre2_compile(pattern->val, pattern_size, options, &errcode, &erroffset, NULL);
+	char *a = pattern->val;
+	//a = mstrcat("/", pattern->val);
+	//a = mstrcat(a, "/");
+	//a = mstrcat(a, ex->val );
+
+	//printf("%s\n", a);
+	//exit(0);
+
+	re = pcre2_compile(a, pattern_size, options, &errcode, &erroffset, NULL);
 	if (re == NULL)
 	{
 		pcre2_get_error_message(errcode, buffer, 120);
@@ -41,7 +49,7 @@ do_regex_exec( variable *ths, variable *subject ) {
 	dovar(k, "re", DOTYPE_STRING );
 	variable * re = array_get( ths, k );
 
-	size_t subject_size = strlen(subject->val);
+	size_t subject_size;//= strlen(subject->val);
 	uint32_t options = 0;
 
 	pcre2_match_data *match_data;
@@ -52,40 +60,86 @@ do_regex_exec( variable *ths, variable *subject ) {
 	PCRE2_SIZE* ovector;
 
 	match_data = pcre2_match_data_create(ovecsize, NULL);
-	rc = pcre2_match(re->val, subject->val, subject_size, 0, options, match_data, NULL);
+	dovar(sl, "lastIndex", DOTYPE_STRING);
+	dovar(sind, "index", DOTYPE_STRING);
+	dovar(slstr, "str", DOTYPE_STRING);
+	
+	char *msub = subject->val;
+
+	variable *lastStr = array_get(ths, slstr );
+	if( lastStr->type != DOTYPE_UNDEF ) {
+		msub = lastStr->val;
+	}
+
+	if( ! msub ) {
+		dovar(rz, 0, DOTYPE_INT);
+		return rz;
+	}
+
+	subject_size = strlen(msub);
+
+	rc = pcre2_match(re->val, msub, subject_size, 0, options, match_data, NULL);
 
 	array *r = safe_alloc_new( &alloc, sizeof( array *) );
 	array_init(r);
 	dovar( arr, r, DOTYPE_ARRAY );
 
 	if(rc == 0) {
+		dovar(ml, 0, DOTYPE_INT);
+		array_set(ths, sl, ml);
 		//fprintf(stderr,"offset vector too small: %d",rc);
 	}
 	else if(rc > 0)
 	{
 
+		variable *lastIndex = array_get(ths, sl );
+		int lindx = 0;
+		if( lastIndex->type != DOTYPE_UNDEF ) {
+			lindx = (int)lastIndex->val;
+		}
+
+		//printf("%d\n", rc);
 		ovector = pcre2_get_ovector_pointer(match_data);
 		PCRE2_SIZE i;
 		for(i = 0; i < rc; i++) {
 
-			PCRE2_SPTR start = subject->val + ovector[2*i];
+			PCRE2_SPTR start = msub + ovector[2*i];
 			PCRE2_SIZE slen = ovector[2*i+1] - ovector[2*i];
 			
 			char *s = safe_alloc_new( &alloc, slen );
 			memcpy(s,start, slen);
 			s[slen] = '\0';
 
+			dovar(ml, lindx+ovector[2*i+1], DOTYPE_INT);
+			
+			dovar(str1, start+slen, DOTYPE_STRING);
+			array_set(ths, slstr, str1);
+
+			array_set(ths, sl, ml);
+
+			dovar(indt, lindx+ovector[2*i], DOTYPE_INT);
+			array_set(ths, sind, indt);
+
 			dovar(k, i, DOTYPE_INT);
 			dovar(v, s, DOTYPE_STRING);
 
 			array_set( arr, k, v );
-		
+
 		}
+	} else {
+		dovar(ml, 0, DOTYPE_INT);
+		array_set(ths, sl, ml);
 	}
 
 
 	pcre2_match_data_free(match_data);
-	pcre2_code_free(re->val);
+	//pcre2_code_free(re->val);
+	//printf("dddd\n");
+	if( r->length == 0 ) {
+		dovar(rz, 0, DOTYPE_INT);
+		return rz;	
+	}
+
 	return arr;
 }
 
--- test/regex.do
+++ test/regex.do
@@ -1,10 +1,11 @@
-//import lib.regexp
-
 main() {
 
-	let r = /(\w+)\s*(\w+)\s*/g.exec("this is test");
+	let regex1 = new RegExp('foo*', 'gmi');
+	let str1 = 'table football, foosball';
 
-	print( r );
-
-
+	print( str1 +"\n\n");
+	
+	while( let array1 = regex1.exec(str1) ) {
+		print("Found "+array1[0]+" Next starts at "+regex1.lastIndex+" ");
+	}
 }
