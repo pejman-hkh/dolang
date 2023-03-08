5b4f014633df1aee1199ddc79fa6bf2a13d02c9f
pejman <pejman.hkh@gmail.com>
2023-03-08 17:58:32 +0330

    e

---
+++ testjs/callback1.js
@@ -0,0 +1,17 @@
+
+var s = "test s";
+var l = func( a ) {
+
+	print("in callback\n");
+	print( a );
+};
+
+l("test\n");
+
+var add = func( a , b ) {
+	return a+b;
+};
+
+
+print( "sum of 2 and 4 is : "+add( 2, 4)+"\n" );
+
