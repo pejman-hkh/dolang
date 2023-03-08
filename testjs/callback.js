09db65484f6027205994ba49d256cf5089b444d8
pejman <pejman.hkh@gmail.com>
2023-03-08 17:56:35 +0330

    working on js

---
+++ testjs/callback.js
@@ -0,0 +1,13 @@
+function each( arr, cb ) {
+	for( var k,v in arr ) {
+		cb( k, v );
+	}
+}
+
+
+let arr = [0 : {'name' : 'second'}, 1 : {'name' : 'test1'} ];
+
+each(arr, func( k, v) {
+	print( k );
+	print( v );
+});
---
+++ testjs/concat.js
@@ -0,0 +1,17 @@
+let str1 = "abc";
+let str2 = "defghijklmnñopqrstuvwxyz";
+
+let a = "test";
+let b = " test1";
+print( a + b );
+
+print( a.length );
+
+let str = "";
+for(let i = 0; i < 30000; i++) {
+  str += str1 + str2;
+}
+
+print( str )
+print("\n")
+print( str.length )
